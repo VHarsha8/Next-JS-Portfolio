a26fb3bc6df8a159e4b88397d0b12b2b1782793a
VHarsha8 <harshavardhan6304@gmail.com>
2024-11-24 15:48:24 +0530

    Nextjs Portfolio

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+
+export default function Home() {
+  return (
+    <div className="bg-black relative h-screen container mx-auto flex flex-col justify-center items-center">
+          <div
+  className="absolute inset-0 h-full w-full bg-transparent bg-[linear-gradient(to_right,#80808028_1px,transparent_1px),linear-gradient(to_bottom,#80808028_1px,transparent_1px)] bg-[size:24px_24px]"
+></div>
+          <div className="flex-col flex gap-3 ">
+                <h1 className="font-mono text-white text-5xl font-medium">Welcome to my first NEXT.js work.</h1>
+                <h1 className="font-mono text-white text-xl ">Hello 👋 I am Harshavardhan</h1>
+                <h1 className="font-mono text-white text-xl ">Web designer</h1>
+          </div>
+          
+    </div>
+  );
+}
